476454fd027721dcfd2b83ef50fb9e9f4a5e3835
ahmedsalehalzabr <ahmedalzabr2@gmail.com>
2024-08-31 23:19:22 -1200

    searchForm for product

--- src/app/modules/products/list-products/list-products.component.ts
+++ src/app/modules/products/list-products/list-products.component.ts
@@ -1,5 +1,6 @@
 import { PagedAndSortedResultRequestDto } from '@abp/ng.core';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 
 import { ProductDto, ProductsService } from '@proxy/products';
@@ -7,20 +8,34 @@ import { ProductDto, ProductsService } from '@proxy/products';
 @Component({
   selector: 'app-list-products',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './list-products.component.html',
   styleUrl: './list-products.component.scss'
 })
 export class ListProductsComponent implements OnInit {
 products : ProductDto[] = [];
 input: PagedAndSortedResultRequestDto = { maxResultCount: 10, skipCount: 0 };
+searchForm: FormGroup;
 
-  constructor(private productsService: ProductsService, private router:Router) {}
+  constructor(private productsService: ProductsService, private router:Router
+              ,private formBuilder:FormBuilder
+  ) {
+    this.buildForm();
+  }
 
   ngOnInit(): void {
-     this.productsService.getList(this.input).subscribe(result => this.products = result.items); 
+    this.searchProducts();
+  }
+  buildForm() {
+    this.searchForm = this.formBuilder.group({
+      filter: new FormControl(''),
+    });
   }
   addProduct() {
     this.router.navigateByUrl('/products/add');
+  }
+
+  searchProducts() {
+    this.productsService.getList(this.searchForm.value).subscribe(result => this.products = result.items);
   }
 }
